dce5bac14bd719ae758b485ddef24133f2a4080a
mikebush92 <58124810+mikebush92@users.noreply.github.com>
2022-03-13 18:21:19 -0400

    press 1 fix
    fixed an issue with it not saying press 1

--- batch file for data streams.bat
+++ batch file for data streams.bat
@@ -1,14 +1,14 @@
 @echo off
 if not exist "users" mkdir  "users"
-echo "Hello">users:test.txt
+echo "Hello World">users:test.txt
 pause
 
-echo to view the file/datastream press anything
+echo to view the file/datastream press 1
 set /p ans="Enter Number:"
 if %ans%==1  (goto 1) else (goto exit)
 
 :1
-notepad users:test.txt
+notepad users:test.tx
 pause
 
 :exit
